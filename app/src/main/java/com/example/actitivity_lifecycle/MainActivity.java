840e862dd8bd9beee69824b3916a4832be5f2251
Aliza Tariq <alizatariq1122@gmail.com>
2022-12-02 20:24:56 +0500

    activity_life_cycle

--- app/src/main/java/com/example/actitivity_lifecycle/MainActivity.java
+++ app/src/main/java/com/example/actitivity_lifecycle/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.actitivity_lifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Activity1","onCreate");
+        button=findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    protected void onStart()
+    {
+        super.onStart();
+        Log.d("Activity1","onStart : ");
+    }
+    
 }
