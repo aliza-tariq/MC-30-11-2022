43c4931b4b0ac7ffdd56ed52b35c8ca8406265d8
Aliza Tariq <alizatariq1122@gmail.com>
2022-12-05 20:51:03 +0500

    ARRAY LIST VIEW

--- app/src/main/java/com/example/actitivity_lifecycle/List_View.java
+++ app/src/main/java/com/example/actitivity_lifecycle/List_View.java
@@ -3,12 +3,29 @@ package com.example.actitivity_lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class List_View extends AppCompatActivity {
+    ArrayList<String> array=new ArrayList<>();
+    ListView list;
+    EditText text;
+    Button buuton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+        array.add("Red");
+        array.add("blue");
+        array.add("green");
+
+        list=findViewById(R.id.mylistview);
+        text=findViewById(R.id.mytext);
+        button=findViewById(R.id.mybutton);
+        Arra
     }
 }
